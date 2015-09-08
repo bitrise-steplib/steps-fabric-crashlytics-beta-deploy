3087139aac9d20e4e4a78df61a41b264dfc2b145
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-09-08 08:26:06 +0200

    update

--- step.sh
+++ step.sh
@@ -35,20 +35,20 @@ set_error_cleanup_function CLEANUP_ON_ERROR_FN
 
 #
 # - Required Params
-if [ -z "${STEP_CRASHLYTICS_API_KEY}" ] ; then
-	finalcleanup '* Required input `$STEP_CRASHLYTICS_API_KEY` not provided!'
+if [ -z "${api_key}" ] ; then
+	finalcleanup '* Required input `$api_key` not provided!'
 	exit 1
 fi
-if [ -z "${STEP_CRASHLYTICS_BUILD_SECRET}" ] ; then
-	finalcleanup '* Required input `$STEP_CRASHLYTICS_BUILD_SECRET` not provided!'
+if [ -z "${build_secret}" ] ; then
+	finalcleanup '* Required input `$build_secret` not provided!'
 	exit 1
 fi
-if [ -z "${STEP_CRASHLYTICS_IPA_PATH}" ] ; then
-	finalcleanup '* Required input `$STEP_CRASHLYTICS_IPA_PATH` not provided!'
+if [ -z "${ipa_path}" ] ; then
+	finalcleanup '* Required input `$ipa_path` not provided!'
 	exit 1
 fi
-if [ ! -f "${STEP_CRASHLYTICS_IPA_PATH}" ] ; then
-	finalcleanup "* IPA path defined but the file does not exist at path: ${STEP_CRASHLYTICS_IPA_PATH}"
+if [ ! -f "${ipa_path}" ] ; then
+	finalcleanup "* IPA path defined but the file does not exist at path: ${ipa_path}"
 	exit 1
 fi	
 
@@ -66,20 +66,20 @@ write_section_to_formatted_output "# Submitting..."
 #
 # - Release Notes: save to file
 CONFIG_release_notes_pth="${HOME}/app_release_notes.txt"
-printf "%s" "${STEP_CRASHLYTICS_RELEASE_NOTES}" > "${CONFIG_release_notes_pth}"
+printf "%s" "${release_notes}" > "${CONFIG_release_notes_pth}"
 
 #
 # - Optional params
 
-if [ ! -z "${STEP_CRASHLYTICS_EMAIL_LIST}" ] ; then
-	_param_emails="-emails \"${STEP_CRASHLYTICS_EMAIL_LIST}\""
+if [ ! -z "${email_list}" ] ; then
+	_param_emails="-emails \"${email_list}\""
 fi
-if [ ! -z "${STEP_CRASHLYTICS_GROUP_ALIASES_LIST}" ] ; then
-	_param_groups="-groupAliases ﻿\"${STEP_CRASHLYTICS_GROUP_ALIASES_LIST}\""
+if [ ! -z "${group_aliases_list}" ] ; then
+	_param_groups="-groupAliases ﻿\"${group_aliases_list}\""
 fi
 
 CONFIG_is_send_notifications="YES"
-if [[ "${STEP_CRASHLYTICS_NOTIFICATION}" == "No" ]] ; then
+if [[ "${notification}" == "No" ]] ; then
 	CONFIG_is_send_notifications="NO"
 fi
 
@@ -87,8 +87,8 @@ fi
 #
 # - Submit
 print_and_do_command_exit_on_error "${THIS_SCRIPT_DIR}/Crashlytics.framework/submit" \
-	"${STEP_CRASHLYTICS_API_KEY}" "${STEP_CRASHLYTICS_BUILD_SECRET}" \
-	-ipaPath "${STEP_CRASHLYTICS_IPA_PATH}" \
+	"${api_key}" "${build_secret}" \
+	-ipaPath "${ipa_path}" \
 	-notesPath "${CONFIG_release_notes_pth}" \
 	-notifications "${CONFIG_is_send_notifications}" \
 	${_param_emails} \
@@ -103,17 +103,17 @@ write_section_to_formatted_output "## Notified testers"
 if [[ "${CONFIG_is_send_notifications}" == "NO" ]] ; then
 	echo_string_to_formatted_output "* You disabled notification sending for this deploy"
 else
-	if [ -z "${STEP_CRASHLYTICS_EMAIL_LIST}" ] ; then
+	if [ -z "${email_list}" ] ; then
 		echo_string_to_formatted_output "* No email list provided"
 	else
-		echo_string_to_formatted_output "* Emails: ${STEP_CRASHLYTICS_EMAIL_LIST}"
+		echo_string_to_formatted_output "* Emails: ${email_list}"
 	fi
-	if [ -z "${STEP_CRASHLYTICS_GROUP_ALIASES_LIST}" ] ; then
+	if [ -z "${group_aliases_list}" ] ; then
 		echo_string_to_formatted_output "* No groups provided"
 	else
-		echo_string_to_formatted_output "* Groups: ${STEP_CRASHLYTICS_GROUP_ALIASES_LIST}"
+		echo_string_to_formatted_output "* Groups: ${group_aliases_list}"
 	fi
 fi
 
 write_section_to_formatted_output "## Release Notes"
-write_section_to_formatted_output "${STEP_CRASHLYTICS_RELEASE_NOTES}"
+write_section_to_formatted_output "${release_notes}"
